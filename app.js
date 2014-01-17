7564ab2ac8cfb9dafabdfdc450c586e84755dd2f
Steven Rogers <stevenr41@hotmail.com>
2014-01-17 11:08:17 -0800

    Added comments and deleted pong.js

--- app.js
+++ app.js
@@ -7,9 +7,9 @@ var server = http.createServer(function(req,res){
 		res.writeHead(200, {'Content-Type': 'text/html'});
 		res.end(data, 'utf-8');
 	});
-}).listen(3000, "127.0.0.1");
+}).listen(3000, "127.0.0.1"); ///////////////////////////////////////////////
 
-console.log("Server started at 127.0.0.1:3000");
+console.log("Server started at 127.0.0.1:3000"); /////////////////////////////////////////////////////////
 
 var io = require('socket.io').listen(server);
 
