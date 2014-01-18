5e5ed804efd4327f79fea05af0f810c213423643
Steven Rogers <stevenr41@hotmail.com>
2014-01-18 09:31:32 -0800

    Added key press handshake between server and client

--- app.js
+++ app.js
@@ -1,40 +1,52 @@
-//checking if this stuff works
+
+// Import the http module
 var http = require("http");
+// Import the file system module
 var fs = require("fs");
 
+// Create a new server
 var server = http.createServer(function(req,res){
 	fs.readFile('./index.html', function(error, data){
 		res.writeHead(200, {'Content-Type': 'text/html'});
 		res.end(data, 'utf-8');
 	});
-}).listen(3000, "127.0.0.1"); ///////////////////////////////////////////////
+}).listen(3000, "127.0.0.1"); ///////////////////////////////////////////////        <<<<<<<<<<< CHANGE THIS DURING PRODUCTION
 
-console.log("Server started at 127.0.0.1:3000"); /////////////////////////////////////////////////////////
+console.log("Server started at 127.0.0.1:3000"); ////////////////////////////        <<<<<<<<<<< CHANGE THIS DURING PRODUCTION
 
+// Import the socket.io module
 var io = require('socket.io').listen(server);
 
 // My variables for this server
-var WIDTH = 600;
+var WIDTH = 600; // The WIDTH and HEIGHT of the 'play field'
 var HEIGHT = 400;
-var P_WIDTH = 20;
+
+var P_WIDTH = 20; // This is the width and height of the ball
 var P_HEIGHT = 80;
+
 var FACE_GAP = 40; //the gap between the front face of the paddle and the side of the canvas
-var B_WIDTH = 10;
+
+var B_WIDTH = 10; // The width and height of the BALL
 var B_HEIGHT = 10;
-var players = [];
+
+var players = []; // This will hold an array of objects for each player. Plan to store the score and data here. the index is the ID
+
 /*
  * Each player will have a number associated with who they are.
  * I also plan to have a score for that player,
  * and an optional username.
  */
-var player_count = 0;
-var player_id_count = 1;
-var p1YPos = HEIGHT/2;
-var p2YPos = HEIGHT/2;
-var ballX = 0;
-var ballY = 0;
-var ballXM = 0;
-var ballYM = 0;
+var player_count = 0; // This keeps track of the current number of connected players
+var player_id_count = 1; // This increments IDs so no connected player has the same ID
+var p1YPos = HEIGHT/2; // This is the Y position of player 1
+var p2YPos = HEIGHT/2; // This is the Y position of player 2
+var ballX = 0; // This is the X location of the ball
+var ballY = 0; // This is the Y location of the ball
+var ballXM = 0; // This is the X momentum of the ball
+var ballYM = 0; //  This is the Y momentum of the ball
+var p1_id = 0; // This is to keep track of what player is currently player1
+var p2_id = 0; // This is to keep track of what player is currently player2
+
 var p1Up = false; //these 4 set to true if the player is holding the appropriate directional button
 var p1Down = false;
 var p2Up = false;
@@ -42,14 +54,17 @@ var p2Down = false;
 
 
 
-// This is where the data is sent back and forth.
+// When a player connects, It will run this function..
 io.sockets.on('connection', function(socket){
 
+	// Add one to the players and the player IDs
 	player_count++;
 	player_id_count++;
+
+	// Log to the console what's going on..
 	console.log('User connected. ' + player_count + ' user(s) present.');
 
-
+	// Emit to the player 
 	socket.emit('initial_connection', { 
 		player_count:player_count,
 		player_id:player_id_count,
@@ -66,41 +81,80 @@ io.sockets.on('connection', function(socket){
 	});
 
 
+	// socket.on('key_update', function(data){
+	// 	if(data.player_id == p1_id){
+	// 		if(data.key_up){
+	// 			p1Up = true;
+	// 		}else{
+	// 			p1Up = false;
+	// 		}
+	// 		if(data.key_down){
+	// 			p1Down = true;
+	// 		}else{
+	// 			p1Down = false;
+	// 		}
+	// 	}else if(data.player_id == p2_id){
+	// 		if(data.key_up){
+	// 			p1Up = true;
+	// 		}else{
+	// 			p1Up = false;
+	// 		}
+	// 		if(data.key_down){
+	// 			p1Down = true;
+	// 		}else{
+	// 			p1Down = false;
+	// 		}
+	// 	}
+	// });
 
+	// This function is for the client to update the UP ARROW key
+	socket.on('up_key', function(data){
+		if(data.player_id == p1_id){
+			if(data.keyPressed){
+				p1Up = true;
+			}else{
+				p1Up = false;
+			}
+		}else if(data.player_id == p2_id){
+			if(data.keyPressed){
+				p2Up = true;
+			}else{
+				p2Up = false;
+			}
+		}
+	});
 
-
-
-
-
-
-
-	socket.on('move_down', function(socket){
-		p1YPos += 10;
-		socket.broadcast.emit('positions',{p1YPos:p1YPos,p2YPos:p2YPos});
-		socket.emit('positions',{p1YPos:p1YPos,p2YPos:p2YPos});
+	socket.on('down_key',function(data){
+		if(data.player_id == p1_id){
+			if(data.keyPressed){
+				p1Down = true;
+			}else{
+				p1Down = false;
+			}
+		}else if(data.player_id == p2_id){
+			if(data.keyPressed){
+				p2Down = true;
+			}else{
+				p2Down = false;
+			}
+		}
 	});
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-	socket.on('move_up', function(socket){
-		p1YPos -= 10;
-		socket.broadcast.emit('positions',{p1YPos:p1YPos,p2YPos:p2YPos});
-		socket.emit('positions',{p1YPos:p1YPos,p2YPos:p2YPos});
-	});
 });
+
+
+
+
+function updatePlayers(){
+	socket.broadcast.emit('positions',{p1YPos:p1YPos,p2YPos:p2YPos});
+}
+
+
+
 
 //ALWAYS PULL BEFORE YOU ADD ANYTHING
 function startGame()
@@ -124,6 +178,7 @@ function mainLoop(randomMove)
 	moveBall(randomMove);
 	movePaddles();
 	checkBallCollision();
+	updatePlayers();
 }
 
 
@@ -158,11 +213,6 @@ function checkBallCollision()
 {
 	
 }
-
-
-
-
-
 
 
 
